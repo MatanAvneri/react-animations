a50c4357dded96ad02c8cd3372ae2bf3026c8a31
matanavneri <matan.av@gmail.com>
2021-03-08 06:10:17 +0200

    add native css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Sidebar from './Sidebar/Sidebar'
+import Native from './Examples/Native/Native'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
-      <div>Content</div>
+      <div className="App__content">
+        <Native />
+      </div>
     </div>
   );
 }
---
+++ src/Examples/Native/Native.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './Native.module.scss'
+
+const Native: React.FC = () => {
+  return (
+    <div className={style.Native} />
+  )
+}
+
+export default Native
