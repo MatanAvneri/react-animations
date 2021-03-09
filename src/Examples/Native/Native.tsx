da879cc2964660f5acacd0141ccacca72b980e58
matanavneri <matan.av@gmail.com>
2021-03-09 10:53:22 +0200

    add native circles

--- src/Examples/Native/Native.tsx
+++ src/Examples/Native/Native.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './Native.module.scss'
 
 const Native: React.FC = () => {
-  return <div className={style.Native} />
+  const [numberOfCircles, setNumberOfCircles] = useState([0])
+  return (
+    <>
+      <button
+        onClick={() => {
+          setNumberOfCircles([...numberOfCircles, numberOfCircles.length])
+        }}
+      >
+        Add circle
+      </button>
+      {numberOfCircles.map((i) => {
+        return <div key={i} className={style.Native} />
+      })}
+    </>
+  )
 }
 
 export default Native
