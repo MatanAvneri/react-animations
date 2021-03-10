f72261f3c764478425256c8b6b55109c9f43d117
matanavneri <matan.av@gmail.com>
2021-03-10 11:29:45 +0200

    greensock responsiveness

--- src/Examples/GreenSock/GreenSock.tsx
+++ src/Examples/GreenSock/GreenSock.tsx
@@ -1,4 +1,5 @@
-import React, { createRef, useEffect, useRef } from 'react'
+import React, { createRef, useEffect, useRef, useState } from 'react'
+import Measure from 'react-measure'
 import { gsap, TimelineLite } from 'gsap'
 import { PixiPlugin } from 'gsap/PixiPlugin'
 import { MotionPathPlugin } from 'gsap/MotionPathPlugin'
@@ -7,11 +8,14 @@ import style from './GreenSock.module.scss'
 gsap.registerPlugin(PixiPlugin, MotionPathPlugin)
 
 const GreenSock: React.FC = () => {
+  const [width, setWidth] = useState(0)
+  const [height, setHeight] = useState(0)
   const circleRef = createRef<HTMLDivElement>()
   const myTween = useRef(new TimelineLite({ paused: true }))
 
   useEffect(() => {
     myTween.current
+      .clear()
       .to(circleRef.current, 0, {
         x: 0,
         y: 0,
@@ -20,22 +24,22 @@ const GreenSock: React.FC = () => {
         backgroundColor: 'blue',
       })
       .to(circleRef.current, 1, {
-        x: 500,
+        x: width - 100,
         y: 0,
         rotation: 180,
         borderRadius: '100%',
         backgroundColor: 'red',
       })
       .to(circleRef.current, 1, {
-        x: 500,
-        y: 500,
+        x: width - 100,
+        y: height - 100,
         rotation: 0,
         borderRadius: 0,
         backgroundColor: 'blue',
       })
       .to(circleRef.current, 1, {
         x: 0,
-        y: 500,
+        y: height - 100,
         rotation: 180,
         borderRadius: '100%',
         backgroundColor: 'red',
@@ -47,14 +51,29 @@ const GreenSock: React.FC = () => {
         borderRadius: 0,
         backgroundColor: 'blue',
       })
-  }, [])
+  }, [width, height])
 
   return (
-    <>
-      <button onClick={() => myTween.current.play().repeat(-1)}>Play</button>
-      <button onClick={() => myTween.current.pause()}>Pause</button>
-      <div ref={circleRef} className={style.GreenSock} />
-    </>
+    <Measure
+      bounds
+      client
+      onResize={({ client }) => {
+        setWidth(client!.width)
+        setHeight(client!.height)
+      }}
+    >
+      {({ measureRef }) => (
+        <div className={style.GreenSock} ref={measureRef}>
+          <div className={style.GreenSock__buttons}>
+            <button onClick={() => myTween.current.play().repeat(-1)}>
+              Play
+            </button>
+            <button onClick={() => myTween.current.pause()}>Pause</button>
+          </div>
+          <div ref={circleRef} className={style.GreenSock__circle} />
+        </div>
+      )}
+    </Measure>
   )
 }
 
