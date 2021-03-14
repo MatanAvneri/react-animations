b61a8eec55db8316bc6870f6c27b9241a196c7e1
matanavneri <matan.av@gmail.com>
2021-03-14 07:28:25 +0200

    framer drag

--- src/Examples/FramerMotion/FramerMotion.tsx
+++ src/Examples/FramerMotion/FramerMotion.tsx
@@ -1,21 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { motion, Variants } from 'framer-motion'
 import style from './FramerMotion.module.scss'
 
-const Modal: React.FC = () => (
-  <motion.div whileHover={{ scale: 1.1 }} className={style.FramerMotion__modal}>
+const Modal: React.FC<{ constraintsRef: React.RefObject<HTMLDivElement> }> = ({
+  constraintsRef,
+}) => (
+  <motion.div
+    drag
+    dragConstraints={constraintsRef}
+    onDragEnd={(e) => console.log('done dragging', e)}
+    whileHover={{ scale: 1.1 }}
+    className={style.FramerMotion__modal}
+  >
     Lorem ipsum dolor sit amet
   </motion.div>
 )
 
 const FramerMotion: React.FC = () => {
   const [isModalOpen, openModal] = useState(false)
+  const constraintsRef = useRef<HTMLDivElement>(null)
   const modalVariants: Variants = {
     hidden: { opacity: 0, height: 0 },
     visible: { opacity: 1, height: 'auto' },
   }
   return (
-    <div className={style.FramerMotion}>
+    <div ref={constraintsRef} className={style.FramerMotion}>
       <button onClick={() => openModal(!isModalOpen)}>Open Modal</button>
       <br />
       <motion.div
@@ -24,7 +33,7 @@ const FramerMotion: React.FC = () => {
         animate={isModalOpen ? 'visible' : 'hidden'}
         variants={modalVariants}
       >
-        <Modal />
+        <Modal constraintsRef={constraintsRef} />
       </motion.div>
       bottom content
     </div>
